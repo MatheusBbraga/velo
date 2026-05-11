95b4a01b16c2164a5ef3dfa2373e2da486b9b597
Matheus Bezerra <mabbraga2001@gmail.com>
2026-05-11 13:41:03 -0300

    trocando de PO para Feature Actions

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '../support/fixtures'
 
 test('webapp deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173')
